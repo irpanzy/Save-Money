14ed48b96ea770225a3e4a2817542e1246da6b83
pasipasipada <irfanoputra27@gmail.com>
2024-11-28 16:36:15 +0700

    feat: add ChartDataHome model for better data management.

---
+++ lib/models/home/chart_data_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChartDataHome {
+  final String category;
+  final double value;
+  final String percentage;
+  final Color color;
+
+  ChartDataHome(this.category, this.value, this.percentage, this.color);
+}

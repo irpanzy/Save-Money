60a94a9116abe0313ba7784dec1efbff83c461f7
pasipasipada <irfanoputra27@gmail.com>
2024-11-28 19:12:56 +0700

    feat: integrate dynamic navigation with BottomNavbar using currentIndex and onTap

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class BottomNavbar extends StatelessWidget {
-  final BuildContext context;
+  final int currentIndex; // Indeks halaman saat ini
+  final Function(int) onTap; // Callback untuk menangani perubahan halaman
 
-  BottomNavbar(this.context);
+  BottomNavbar({required this.currentIndex, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +29,24 @@ class BottomNavbar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           IconButton(
-            icon: Icon(Icons.home, color: Colors.black),
-            onPressed: () {
-              Navigator.pushNamed(context, '/home');
-            },
+            icon: Icon(
+              Icons.home,
+              color: currentIndex == 0 ? Colors.black : Colors.grey, // Warna berdasarkan halaman aktif
+            ),
+            onPressed: () => onTap(0), // Panggil callback dengan indeks 0
           ),
           IconButton(
-            icon: Icon(Icons.history, color: Colors.grey),
-            onPressed: () {
-              Navigator.pushNamed(context, '/history');
-            },
+            icon: Icon(
+              Icons.history,
+              color: currentIndex == 1 ? Colors.black : Colors.grey,
+            ),
+            onPressed: () => onTap(1),
           ),
           Container(
-            width: 50, 
-            height: 55, 
+            width: 50,
+            height: 55,
             decoration: BoxDecoration(
-              color: Color(0xFFB2E600), 
+              color: Color(0xFFB2E600),
               shape: BoxShape.circle,
               boxShadow: [
                 BoxShadow(
@@ -56,24 +59,24 @@ class BottomNavbar extends StatelessWidget {
             ),
             child: Center(
               child: IconButton(
-                icon: Icon(Icons.add, color: Colors.white, size: 20), 
-                onPressed: () {
-                  Navigator.pushNamed(context, '/add');
-                },
+                icon: Icon(Icons.add, color: Colors.white, size: 20),
+                onPressed: () => onTap(2), // Halaman khusus tombol tambah
               ),
             ),
           ),
           IconButton(
-            icon: Icon(Icons.category, color: Colors.grey),
-            onPressed: () {
-              Navigator.pushNamed(context, '/statistik');
-            },
+            icon: Icon(
+              Icons.category,
+              color: currentIndex == 3 ? Colors.black : Colors.grey,
+            ),
+            onPressed: () => onTap(3),
           ),
           IconButton(
-            icon: Icon(Icons.settings, color: Colors.grey),
-            onPressed: () {
-              Navigator.pushNamed(context, '/setting');
-            },
+            icon: Icon(
+              Icons.settings,
+              color: currentIndex == 4 ? Colors.black : Colors.grey,
+            ),
+            onPressed: () => onTap(4),
           ),
         ],
       ),

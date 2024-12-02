6dc452fcecbd50c3c1df939be243fdbc8b33275f
Yogihafidh <hafidhyogi@gmail.com>
2024-12-02 20:58:49 +0700

    feat: add buttom navbar

--- lib/screens/statistik_screen.dart
+++ lib/screens/statistik_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import '../helpers/navigation_helper.dart';
-import '../widgets/bottom_navbar.dart';
+import 'package:project_apk_catatan_keuangan/helpers/navigation_helper.dart';
+import 'package:project_apk_catatan_keuangan/style/text_style.dart';
+import 'package:project_apk_catatan_keuangan/widgets/bottom_navbar.dart';
+import 'package:project_apk_catatan_keuangan/widgets/home/doughnut_chart_widget.dart';
+import 'package:project_apk_catatan_keuangan/widgets/statistic/category_card.dart';
 
 class StatistikScreen extends StatefulWidget {
   const StatistikScreen({super.key});
 
   @override
-  State<StatistikScreen> createState() => _StatistikScreenState();
+  _StatistikScreenState createState() => _StatistikScreenState();
 }
 
 class _StatistikScreenState extends State<StatistikScreen> {
+  bool isIncomeSelected = true;
   int _currentIndex = 3;
 
   void _onTap(int index) {
@@ -22,17 +26,159 @@ class _StatistikScreenState extends State<StatistikScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFCCFF00),
-      body: Center(
-        child: Text(
-          'Statistik Screen',
-          style: TextStyle(fontSize: 24, fontFamily: "Poppins_SemiBold"),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () {},
+            ),
+            Text(
+              'Statistik',
+              style: TypographyStyle.h4,
+            ),
+          ],
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                elevation: 0,
+                side: const BorderSide(color: Colors.black),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 8.0),
+              ),
+              child: const Text(
+                'May 2024 >',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomNavbar(
-        currentIndex: _currentIndex,
-        onTap: _onTap,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isIncomeSelected = true;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            'Pemasukan',
+                            style: TypographyStyle.h5,
+                          ),
+                          if (isIncomeSelected)
+                            Container(
+                              height: 2,
+                              width: 120,
+                              color: Colors.black,
+                            ),
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isIncomeSelected = false;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            'Pengeluaran',
+                            style: TypographyStyle.h5,
+                          ),
+                          if (!isIncomeSelected)
+                            Container(
+                              height: 2,
+                              width: 120,
+                              color: Colors.black,
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                DoughnutChart(),
+                const SizedBox(height: 30),
+                ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  children: isIncomeSelected
+                      ? _buildIncomeCategoryList()
+                      : _buildExpenseCategoryList(),
+                ),
+              ],
+            ),
+          ),
+          BottomNavbar(
+            currentIndex: _currentIndex,
+            onTap: _onTap,
+          ),
+        ],
       ),
     );
+  }
+
+  List<Widget> _buildIncomeCategoryList() {
+    return [
+      CategoryCard(
+        title: 'Gajihan',
+        percentage: 52,
+        amount: '200.000 IDR',
+        isIncomeSelected: isIncomeSelected,
+      ),
+      CategoryCard(
+        title: 'Bonus THR',
+        percentage: 25,
+        amount: '120.000 IDR',
+        isIncomeSelected: isIncomeSelected,
+      ),
+      CategoryCard(
+        title: 'Freelance',
+        percentage: 12,
+        amount: '120.000 IDR',
+        isIncomeSelected: isIncomeSelected,
+      ),
+      CategoryCard(
+        title: 'Olshop',
+        percentage: 5,
+        amount: '120.000 IDR',
+        isIncomeSelected: isIncomeSelected,
+      ),
+    ];
+  }
+
+  List<Widget> _buildExpenseCategoryList() {
+    return [
+      CategoryCard(
+        title: 'Makanan',
+        percentage: 40,
+        amount: '920.000 IDR',
+        isIncomeSelected: isIncomeSelected,
+      ),
+      CategoryCard(
+        title: 'Fashion',
+        percentage: 40,
+        amount: '220.000 IDR',
+        isIncomeSelected: isIncomeSelected,
+      ),
+    ];
   }
 }
--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,84 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:project_apk_catatan_keuangan/style/color_style.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class BottomNavbar extends StatelessWidget {
-  final int currentIndex; 
-  final Function(int) onTap; 
+  final int currentIndex;
+  final Function(int) onTap;
 
   BottomNavbar({required this.currentIndex, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(24),
-          topRight: Radius.circular(24),
+    return Positioned(
+      bottom: 16, // Jarak navbar dari bawah layar
+      left: 16,
+      right: 16,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: ColorStyle.borderBlackLow, // Warna border
+            width: 1, // Lebar border
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.07),
+              blurRadius: 10,
+              offset: Offset(0, 6),
+            ),
+          ],
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 2,
-            blurRadius: 8,
-            offset: Offset(0, -2),
-          ),
-        ],
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: Icon(
-              Icons.home,
-              color: currentIndex == 0 ? Colors.black : Colors.grey, 
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(
+                Symbols.home_rounded,
+                weight: 600,
+                color: currentIndex == 0
+                    ? ColorStyle.iconActive
+                    : ColorStyle.iconNotActive,
+              ),
+              onPressed: () => onTap(0),
             ),
-            onPressed: () => onTap(0), 
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.history,
-              color: currentIndex == 1 ? Colors.black : Colors.grey,
+            IconButton(
+              icon: Icon(
+                Symbols.history_rounded,
+                weight: 600,
+                color: currentIndex == 1
+                    ? ColorStyle.iconActive
+                    : ColorStyle.iconNotActive,
+              ),
+              onPressed: () => onTap(1),
             ),
-            onPressed: () => onTap(1),
-          ),
-          Container(
-            width: 50,
-            height: 55,
-            decoration: BoxDecoration(
-              color: Color(0xFFB2E600),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 2,
-                  blurRadius: 8,
-                  offset: Offset(0, 0),
+            Container(
+              width: 50,
+              height: 55,
+              decoration: BoxDecoration(
+                color: ColorStyle.primaryColor60,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: IconButton(
+                  icon: Icon(
+                    Symbols.add_rounded,
+                    color: Colors.white,
+                    size: 20,
+                    weight: 600,
+                  ),
+                  onPressed: () => onTap(2),
                 ),
-              ],
-            ),
-            child: Center(
-              child: IconButton(
-                icon: Icon(Icons.add, color: Colors.white, size: 20),
-                onPressed: () => onTap(2), 
               ),
             ),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.category,
-              color: currentIndex == 3 ? Colors.black : Colors.grey,
+            IconButton(
+              icon: Icon(
+                Symbols.category_rounded,
+                weight: 600,
+                color: currentIndex == 3
+                    ? ColorStyle.iconActive
+                    : ColorStyle.iconNotActive,
+              ),
+              onPressed: () => onTap(3),
             ),
-            onPressed: () => onTap(3),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.settings,
-              color: currentIndex == 4 ? Colors.black : Colors.grey,
+            IconButton(
+              icon: Icon(
+                Symbols.settings,
+                weight: 600,
+                color: currentIndex == 4
+                    ? ColorStyle.iconActive
+                    : ColorStyle.iconNotActive,
+              ),
+              onPressed: () => onTap(4),
             ),
-            onPressed: () => onTap(4),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

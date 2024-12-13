adc2addd135a6c6ba6eabc8be06b400beb843385
SatriaAriq <satriaariq121@gmail.com>
2024-12-13 15:56:20 +0700

    Refactor: Update UI Setiings Screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,52 +2,38 @@ import 'package:flutter/material.dart';
 import '../helpers/navigation_helper.dart';
 import '../widgets/bottom_navbar.dart';
 
-void main() {
-  runApp(MyApp());
-}
+class SettingsScreen extends StatelessWidget {
+  final int currentIndex;
+  final bool isDarkMode;
+  final String selectedLanguage;
+  final String selectedCurrency;
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system, // Default to system theme
-      home: SettingsPage(),
-    );
-  }
-}
-
-class SettingsPage extends StatefulWidget {
-  @override
-  _SettingsPageState createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
-  bool isDarkMode = false;
-  String selectedLanguage = 'Indonesia';
-  String selectedCurrency = 'Rupiah';
+  const SettingsScreen({
+    super.key,
+    this.currentIndex = 4,
+    this.isDarkMode = false,
+    this.selectedLanguage = 'Indonesia',
+    this.selectedCurrency = 'Rupiah',
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
-        backgroundColor: Color(0xFFCCFF00),
+        backgroundColor: const Color(0xFFCCFF00),
         elevation: 0,
       ),
       body: ListView(
         children: [
           Container(
-            color: Color(0xFFCCFF00),
-            padding: EdgeInsets.symmetric(vertical: 20),
+            color: const Color(0xFFCCFF00),
+            padding: const EdgeInsets.symmetric(vertical: 20),
             child: Column(
-              children: [
+              children: const [
                 CircleAvatar(
                   radius: 40,
                   backgroundImage: AssetImage(
-                      'assets/profile.jpg'), // Ganti dengan path gambar Anda
+                      'assets/images/ganesha.png'), // Ganti dengan path gambar Anda
                 ),
                 SizedBox(height: 10),
                 Text(
@@ -58,71 +44,78 @@ class _SettingsPageState extends State<SettingsPage> {
             ),
           ),
           ListTile(
-            title: Text('Tema Gelap'),
+            title: const Text('Tema Gelap'),
             trailing: Switch(
               value: isDarkMode,
               onChanged: (value) {
-                setState(() {
-                  isDarkMode = value;
-                });
+                _showNotEditableDialog(context);
               },
             ),
           ),
           ListTile(
-            title: Text('Bahasa'),
+            title: const Text('Bahasa'),
             trailing: Text(selectedLanguage),
             onTap: () async {
-              String? result = await showModalBottomSheet<String>(
+              await showModalBottomSheet<String>(
                 context: context,
                 builder: (context) =>
                     LanguageSelector(selectedLanguage: selectedLanguage),
               );
-              if (result != null) {
-                setState(() {
-                  selectedLanguage = result;
-                });
-              }
             },
           ),
           ListTile(
-            title: Text('Mata Uang'),
+            title: const Text('Mata Uang'),
             trailing: Text(selectedCurrency),
             onTap: () async {
-              String? result = await showModalBottomSheet<String>(
+              await showModalBottomSheet<String>(
                 context: context,
                 builder: (context) =>
                     CurrencySelector(selectedCurrency: selectedCurrency),
               );
-              if (result != null) {
-                setState(() {
-                  selectedCurrency = result;
-                });
-              }
             },
           ),
-          ListTile(
+          const ListTile(
             title: Text('Beri Penilaian'),
             trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // Navigasi ke halaman beri penilaian
-            },
+            onTap: null, // Navigasi ke halaman beri penilaian
           ),
-          ListTile(
+          const ListTile(
             title: Text('Bantuan'),
             trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // Navigasi ke halaman bantuan
-            },
+            onTap: null, // Navigasi ke halaman bantuan
           ),
-          ListTile(
+          const ListTile(
             title: Text('Tentang'),
             trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // Navigasi ke halaman tentang
-            },
+            onTap: null, // Navigasi ke halaman tentang
           ),
         ],
       ),
+      bottomNavigationBar: BottomNavbar(
+        currentIndex: currentIndex,
+        onTap: (index) => NavigationHelper.navigateTo(index, context),
+      ),
+    );
+  }
+
+  void _showNotEditableDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Info'),
+          content: const Text(
+              'Pengaturan ini tidak dapat diubah langsung dalam mode Stateless.'),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -137,13 +130,15 @@ class LanguageSelector extends StatelessWidget {
     return ListView(
       children: [
         ListTile(
-          title: Text('Indonesia'),
-          trailing: selectedLanguage == 'Indonesia' ? Icon(Icons.check) : null,
+          title: const Text('Indonesia'),
+          trailing:
+              selectedLanguage == 'Indonesia' ? const Icon(Icons.check) : null,
           onTap: () => Navigator.pop(context, 'Indonesia'),
         ),
         ListTile(
-          title: Text('English'),
-          trailing: selectedLanguage == 'English' ? Icon(Icons.check) : null,
+          title: const Text('English'),
+          trailing:
+              selectedLanguage == 'English' ? const Icon(Icons.check) : null,
           onTap: () => Navigator.pop(context, 'English'),
         ),
       ],
@@ -161,13 +156,15 @@ class CurrencySelector extends StatelessWidget {
     return ListView(
       children: [
         ListTile(
-          title: Text('Rupiah'),
-          trailing: selectedCurrency == 'Rupiah' ? Icon(Icons.check) : null,
+          title: const Text('Rupiah'),
+          trailing:
+              selectedCurrency == 'Rupiah' ? const Icon(Icons.check) : null,
           onTap: () => Navigator.pop(context, 'Rupiah'),
         ),
         ListTile(
-          title: Text('Dollar US'),
-          trailing: selectedCurrency == 'Dollar US' ? Icon(Icons.check) : null,
+          title: const Text('Dollar US'),
+          trailing:
+              selectedCurrency == 'Dollar US' ? const Icon(Icons.check) : null,
           onTap: () => Navigator.pop(context, 'Dollar US'),
         ),
       ],

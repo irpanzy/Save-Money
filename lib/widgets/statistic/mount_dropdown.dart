fcdbe1bed184140820204d9e66a3928ac94810ad
rickyrev <132206679+rickyrev@users.noreply.github.com>
2024-12-13 16:51:51 +0700

    Update mount_dropdown.dart

--- lib/widgets/statistic/mount_dropdown.dart
+++ lib/widgets/statistic/mount_dropdown.dart
@@ -9,47 +9,95 @@ class MonthSelector extends StatefulWidget {
   _MonthSelectorState createState() => _MonthSelectorState();
 }
 
-class _MonthSelectorState extends State<MonthSelector> {
-  String selectedMonth = 'May 2024';
+List<String> months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
-  List<String> months = [
-    'May 2024',
-    'June 2024',
-    'July 2024',
-    'August 2024',
-    'September 2024',
-    'October 2024',
-  ];
+class _MonthSelectorState extends State<MonthSelector> {
+  final currentTime = DateTime.now();
+  String selectedMonth = months[DateTime.now().month - 1];
+  String selectedYear = "${DateTime.now().year}";
+
+  List<int> futureYears = List.generate(10, (i) => 2025 + i);
+  List<int> pastPresentYears = List.generate(10, (i) => 2024 - i);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        child: DropdownButton<String>(
-      value: selectedMonth,
-      onChanged: (String? newMonth) {
-        setState(() {
-          selectedMonth = newMonth ?? selectedMonth;
-        });
-      },
-      items: months.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            style: TypographyStyle.l1Regular,
+    List<String> listYearAvailable =
+        (futureYears.reversed.toList() + pastPresentYears).map((x) => '$x').toList();
+
+    return Row(
+      children: [
+        DropdownButton<String>(
+          value: selectedMonth,
+          onChanged: (String? newMonth) {
+            setState(() {
+              selectedMonth = newMonth ?? selectedMonth;
+            });
+          },
+          items: months.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+                style: TypographyStyle.l1Regular,
+              ),
+            );
+          }).toList(),
+          underline: Container(),
+          dropdownColor: Colors.white,
+          icon: const Icon(
+            Symbols.arrow_forward_ios_rounded,
+            color: Colors.black,
+            size: 16,
+            weight: 600,
           ),
-        );
-      }).toList(),
-      underline: Container(),
-      dropdownColor: Colors.white,
-      icon: const Icon(
-        Symbols.arrow_forward_ios_rounded,
-        color: Colors.black,
-        size: 16,
-        weight: 600,
-      ),
-      alignment: Alignment.center,
-      isDense: true,
-    ));
+          alignment: Alignment.center,
+          isDense: true,
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        DropdownButton<String>(
+          value: selectedYear,
+          onChanged: (String? newMonth) {
+            setState(() {
+              selectedYear = newMonth ?? selectedYear;
+            });
+          },
+          items:
+              listYearAvailable.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+                style: TypographyStyle.l1Regular,
+              ),
+            );
+          }).toList(),
+          underline: Container(),
+          dropdownColor: Colors.white,
+          icon: const Icon(
+            Symbols.arrow_forward_ios_rounded,
+            color: Colors.black,
+            size: 16,
+            weight: 600,
+          ),
+          alignment: Alignment.center,
+          isDense: true,
+        ),
+      ],
+    );
   }
 }
